52a421dc6ad35a1651f18cf803e16fa0867d4a06
Francisco Raposo <fraposo@redhat.com>
2024-10-20 19:07:46 -0300

    OpenTelemetry + Grafana with Quarkus and Spring Boot

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 ## Quarkus observability
 oc new-project quarkus-observability
-oc create -f ./quarkus-observability/app-openshift-java17.yaml
-oc create -n quarkus-observability -f ./quarkus-observability/monitoring-openshift.yaml
-oc create -n grafana -f ./grafana/operator-deploy.yaml
+oc apply -f ./quarkus-observability/app-openshift-java17.yaml
+oc apply -n quarkus-observability -f ./quarkus-observability/monitoring-openshift.yaml
+oc create -n grafana-operator -f ./grafana/operator-deploy.yaml
 
 ## Spring Boot observability
 oc new-project spring-observability
 oc new-app --name spring-opentelemetry https://github.com/panchoraposo/ocp-java-observability.git --context-dir=springboot-observability
-oc create -n spring-observability -f ./springboot-observability/monitoring.yaml
+oc apply -n spring-observability -f ./springboot-observability/monitoring.yaml
 oc create -n grafana-operator -f ./grafana/operator-deploy.yaml
 
 ## LokiStack
